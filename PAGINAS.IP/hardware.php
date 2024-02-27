f6ba4595c32b9fdd8d127c6a3b708e7b7d3273ef
jeseniaisabel231 <150484680+JohnMata0427@users.noreply.github.com>
2024-02-27 01:46:19 -0500

    pantalla nueva

--- PAGINAS.IP/hardware.php
+++ PAGINAS.IP/hardware.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&family=Volkhov:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="hardware.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
@@ -16,12 +19,11 @@
     <div>
         <img class="img-1" src="../img/fondo2.png" alt="Tarjeta Grafica">
     </div>
-
     <main>
-        <section class="categoria">
-            <h2>Categoria</h2>
+        <section class="categoria_h">
+            <h2 class="letra_filtrar">Filtrar por subcategorias</h2>
             <h1>Hadware</h1>
-            <div class="hadware_tipos">
+            <div class="hardware_tipos_h">
                 <ul>
                     <li>Procesadores</li>
                     <li>Mainboards</li>
@@ -31,29 +33,29 @@
                     <li>Fuentes de poder</li>
                 </ul>
             </div>
-            <div class="filtrar_precio">
+            <hr class="hr_h">
+            <div class="filtrar_precio_h">
                 <h2>Filtrar por precio</h2>
-                <hr>
                 <p>Precio: $0 - $2820</p>
                 <button>Filtrar</button>
             </div>
         </section>
-        <section class="productos_nuevos">
-            <div class="contenedor-principal">
-                <h2 class="h2_productos">Productos Nuevos</h2>
-                <div class="contenedor-productos">
-                    <div class="producto_1">
-                        <div class="img_producto">
+<!--PRODUCTOS------------------------------------------------------------------------------------------------------------- -->
+        <section class="productos_nuevos_h">
+            <div class="contenedor-principal_h">
+                <div class="contenedor-productos_h">
+                    <div class="producto_1_h">
+                        <div class="img_producto_h">
                             <img src="../img/producto_1.png" alt="">
                         </div><hr>
-                        <div class="descripcion_producto">
-                            <p class="codigo">SKU:PC42788</p><br>
-                            <p class="nombre">PC GAMER</p><br>
-                            <p class="precio">$ 904.32</p>
+                        <div class="descripcion_producto_h">
+                            <p class="codigo_h">SKU:PC42788</p><br>
+                            <p class="nombre_h">PC GAMER</p><br>
+                            <p class="precio_h">$ 904.32</p>
 
-                            <input class="cantidad" type="number">
+                            <input class="cantidad_h" type="number">
                             
-                            <button class="compras" href=""><svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <button class="compras_h" href=""><svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g id="Group 39">
                                 <path id="Vector" d="M7.94084 21.7019C8.91542 21.7019 9.70548 20.8922 9.70548 19.8934C9.70548 18.8946 8.91542 18.0849 7.94084 18.0849C6.96626 18.0849 6.17621 18.8946 6.17621 19.8934C6.17621 20.8922 6.96626 21.7019 7.94084 21.7019Z" fill="white"/>
                                 <path id="Vector_2" d="M20.2932 21.7019C21.2678 21.7019 22.0578 20.8922 22.0578 19.8934C22.0578 18.8946 21.2678 18.0849 20.2932 18.0849C19.3186 18.0849 18.5286 18.8946 18.5286 19.8934C18.5286 20.8922 19.3186 21.7019 20.2932 21.7019Z" fill="white"/>
@@ -65,18 +67,18 @@
                         
                     </div>
     
-                    <div class="producto_1">
-                        <div class="img_producto">
+                    <div class="producto_1_h">
+                        <div class="img_producto_h">
                             <img src="../img/producto_2.png" alt="">
                         </div><hr>
-                        <div class="descripcion_producto">
-                            <p class="codigo">SKU:PC42788</p><br>
-                            <p class="nombre">PCGAMER</p><br>
-                            <p class="precio">$ 904.32</p>
+                        <div class="descripcion_producto_h">
+                            <p class="codigo_h">SKU:PC42788</p><br>
+                            <p class="nombre_h">PCGAMER</p><br>
+                            <p class="precio_h">$ 904.32</p>
 
-                            <input class="cantidad" type="number">
+                            <input class="cantidad_h" type="number">
                             
-                            <button class="compras"href=""><svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <button class="compras_h"href=""><svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g id="Group 39">
                                 <path id="Vector" d="M7.94084 21.7019C8.91542 21.7019 9.70548 20.8922 9.70548 19.8934C9.70548 18.8946 8.91542 18.0849 7.94084 18.0849C6.96626 18.0849 6.17621 18.8946 6.17621 19.8934C6.17621 20.8922 6.96626 21.7019 7.94084 21.7019Z" fill="white"/>
                                 <path id="Vector_2" d="M20.2932 21.7019C21.2678 21.7019 22.0578 20.8922 22.0578 19.8934C22.0578 18.8946 21.2678 18.0849 20.2932 18.0849C19.3186 18.0849 18.5286 18.8946 18.5286 19.8934C18.5286 20.8922 19.3186 21.7019 20.2932 21.7019Z" fill="white"/>
@@ -88,18 +90,18 @@
                         
                     </div>
     
-                    <div class="producto_1">
-                        <div class="img_producto">
-                            <img src="../img/producto_3.png" alt="">
+                    <div class="producto_1_h">
+                        <div class="img_producto_h">
+                            <img src="../img/producto_2.png" alt="">
                         </div><hr>
-                        <div class="descripcion_producto">
-                            <p class="codigo">SKU:PC42788</p><br>
-                            <p class="nombre">PCGAMER</p><br>
-                            <p class="precio">$ 904.32</p>
+                        <div class="descripcion_producto_h">
+                            <p class="codigo_h">SKU:PC42788</p><br>
+                            <p class="nombre_h">PCGAMER</p><br>
+                            <p class="precio_h">$ 904.32</p>
 
-                            <input class="cantidad" type="number">
+                            <input class="cantidad_h" type="number">
                             
-                            <button class="compras" href=""><svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <button class="compras_h"href=""><svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g id="Group 39">
                                 <path id="Vector" d="M7.94084 21.7019C8.91542 21.7019 9.70548 20.8922 9.70548 19.8934C9.70548 18.8946 8.91542 18.0849 7.94084 18.0849C6.96626 18.0849 6.17621 18.8946 6.17621 19.8934C6.17621 20.8922 6.96626 21.7019 7.94084 21.7019Z" fill="white"/>
                                 <path id="Vector_2" d="M20.2932 21.7019C21.2678 21.7019 22.0578 20.8922 22.0578 19.8934C22.0578 18.8946 21.2678 18.0849 20.2932 18.0849C19.3186 18.0849 18.5286 18.8946 18.5286 19.8934C18.5286 20.8922 19.3186 21.7019 20.2932 21.7019Z" fill="white"/>
@@ -111,18 +113,18 @@
                         
                     </div>
     
-                    <div class="producto_1">
-                        <div class="img_producto">
-                            <img src="../img/producto_4.png" alt="">
+                    <div class="producto_1_h">
+                        <div class="img_producto_h">
+                            <img src="../img/producto_2.png" alt="">
                         </div><hr>
-                        <div class="descripcion_producto">
-                            <p class="codigo">SKU:PC42788</p><br>
-                            <p class="nombre">PCGAMER</p><br>
-                            <p class="precio">$ 904.32</p>
+                        <div class="descripcion_producto_h">
+                            <p class="codigo_h">SKU:PC42788</p><br>
+                            <p class="nombre_h">PCGAMER</p><br>
+                            <p class="precio_h">$ 904.32</p>
 
-                            <input class="cantidad" type="number">
+                            <input class="cantidad_h" type="number">
                             
-                            <button class="compras" href=""><svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <button class="compras_h"href=""><svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g id="Group 39">
                                 <path id="Vector" d="M7.94084 21.7019C8.91542 21.7019 9.70548 20.8922 9.70548 19.8934C9.70548 18.8946 8.91542 18.0849 7.94084 18.0849C6.96626 18.0849 6.17621 18.8946 6.17621 19.8934C6.17621 20.8922 6.96626 21.7019 7.94084 21.7019Z" fill="white"/>
                                 <path id="Vector_2" d="M20.2932 21.7019C21.2678 21.7019 22.0578 20.8922 22.0578 19.8934C22.0578 18.8946 21.2678 18.0849 20.2932 18.0849C19.3186 18.0849 18.5286 18.8946 18.5286 19.8934C18.5286 20.8922 19.3186 21.7019 20.2932 21.7019Z" fill="white"/>
